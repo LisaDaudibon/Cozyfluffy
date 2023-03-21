ee5d2fd05c7f701a89aefefc1edd0e3c9971cb7d
KarineDHoshi <karinedhoshi@yahoo.com>
2023-03-21 18:56:07 +0100

    KD Admin can add user with email and password

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
    root "home#index"
-  resources :admin, only: [:index]
+  resources :admin, only: %i[index create new]
 end
