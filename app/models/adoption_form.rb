2b6928fd1944bc72b485b5e6d541d0e4cd2a5027
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-03-21 18:06:06 +0100

    LD relations and validations adoption_form models

--- app/models/adoption_form.rb
+++ app/models/adoption_form.rb
@@ -1,2 +1,12 @@
 class AdoptionForm < ApplicationRecord
+  belongs_to :cat
+
+  validates :first_name, presence: true { message: "On a besoin de ton prénom xoxo" }
+  validates :last_name, presence: true { message: "On a besoin de ton nom xoxo" }
+  validates :email, 
+    presence: true { message: "Il faut un email pour qu'on puisse te recontacter !" }
+    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Ton email n'a pas le bon format mon coco ! " }
+  validates :phone_number, 
+    uniqueness: true,
+    presence: true { message: "On a besoin de ton numéro de téléphone pour te contacter ! Promis, on fera pas de spam" }
 end
