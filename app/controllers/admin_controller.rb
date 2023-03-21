44186166a95226d2e67305948e4b0dbce2a86df0
KarineDHoshi <karinedhoshi@yahoo.com>
2023-03-21 12:13:34 +0100

    KD is admin authentication

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,16 @@
 class AdminController < ApplicationController
+before_action :authenticate_user!
+before_action :require_is_admin, only: [:index]
+      
+def index
 end
+      
+private
+      
+  def require_is_admin
+    unless current_user.is_admin?
+     redirect_to root_path, alert: "Access denied."
+   end
+  end
+end      
+
