2c61601a03bb7dbf94afd4260cea504892c7aad2
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-03-21 16:24:07 +0100

    LD validation Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  validates :title, presence: true: { message: "Ajoute un titre à ton évènement, enfin voyons ;)" }
+  validates :date, presence: true: { message: "Les visiteurs ont besoin d'une date ! Il faut les prévenir de la fin du monde :P" }
 end
