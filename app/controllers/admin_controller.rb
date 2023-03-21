9575d3e96b95257e1edbbe8ff1eae9e29cdeb996
KarineDHoshi <karinedhoshi@yahoo.com>
2023-03-21 12:28:29 +0100

    KD generate Devise:views to test admin panel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -9,7 +9,7 @@ private
       
   def require_is_admin
     unless current_user.is_admin?
-     redirect_to root_path, alert: "Access denied."
+     #redirect_to root_path, alert: "Access denied."
    end
   end
 end      
