0b977a0f5235f963f1c9559c53541728da012a0e
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-03-22 20:29:58 +0100

    LD routes menu updated

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :admin, only: %i[index new]
   resources :cats, only: %i[index new show edit]
   resources :association, only: %i[index new show edit]
-  resources :menu, only: %i[index new show edit]
+  resources :menu, only: %i[index new create edit update destroy]
 end
