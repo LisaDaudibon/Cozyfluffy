a1e1c8f04f0e9f3da148e1a67868039f4c7b7a23
KarineDHoshi <karinedhoshi@yahoo.com>
2023-03-22 17:26:43 +0100

    KD checkout for admin for new user and adoptable cat in creation form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root "home#index"
-  resources :admin, only: %i[index new]
+  resources :admin, only: %i[index new create ] 
   resources :cats, only: %i[index new show edit create]
   resources :association, only: %i[index new show edit]
   resources :menu, only: %i[index new show edit]
