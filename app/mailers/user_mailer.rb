c246cd3d17ec5391812147f380e4dd766f94086a
KarineDHoshi <karinedhoshi@yahoo.com>
2023-03-22 21:04:15 +0100

    KD user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,6 +10,6 @@ class UserMailer < ApplicationMailer
       @url  = "http://localhost:3000"
   
       # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-      mail(to: @user.email, subject: 'Bienvenue chez nous !')
+      mail(to: @user.email, subject: 'Bienvenue chez nous !') 
     end
 end
