94ccfb484bb36e2452bc02888d80c307b4c06d4b
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-03-23 21:20:01 +0100

    LD index page controller menu(method products)

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -5,20 +5,46 @@ class MenuController < ApplicationController
   end
 
   def new
+    @product = Product.new
   end
 
   def create 
-
+    @product = Product.create(product_params)
+    if @product.save 
+      redirect_to admin_index_path 
+    else
+      flash[:error] = "Echec d'ajout de minou!"
+      render :new
+    end
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
-
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:success] = "Le produit a bien été modifié !"
+      redirect_to cat_path(:id)
+    else
+      flash[:danger] = "Erreur : ton formulaire n'était pas correct. Le titre ne doit pas faire moins de 3 chars ou plus de 14 ! Et il doit y avoir un contenu !"
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   def destroy
-
+    @product.find = Product.find(params[:id])
+    @product.destroy
+    flash[:success] = "Le produit a été supprimé avec succès !"
+    redirect_to root_path
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :category_id)
+  end
+
+
 end
