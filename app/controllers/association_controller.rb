f8b608de5ef887481a95e670dcdd040eb48bda78
Suziwan <christe.suzanne@gmail.com>
2023-03-24 12:16:14 +0000

    SC update controller and views asso

--- app/controllers/association_controller.rb
+++ app/controllers/association_controller.rb
@@ -8,17 +8,38 @@ class AssociationController < ApplicationController
   end
 
   def new
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.user = User.first
+    
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
   def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to root_path
   end
 
   def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to root_path
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :date, :description) 
   end
 end
