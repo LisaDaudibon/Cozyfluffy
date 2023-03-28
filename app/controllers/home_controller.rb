e71eb311a2647173141e4e1332b77424dcd340e5
KarineDHoshi <karinedhoshi@yahoo.com>
2023-03-28 15:02:33 +0200

    KD event show everyday

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
-		@event = Event.where(
+		@events = Event.where(
 		date: Time.now.beginning_of_month.beginning_of_week..Time.now.end_of_month.end_of_week)
 	end 
 end
