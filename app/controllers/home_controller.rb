adc48fa65129ea19adf9ff4b302573e4105aad79
KarineDHoshi <karinedhoshi@yahoo.com>
2023-03-28 13:53:05 +0200

    KD add Gem simple_calendar and controller home index method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
 	def index
+		@event = Event.where(
+		date: Time.now.beginning_of_month.beginning_of_week..Time.now.end_of_month.end_of_week)
 	end 
 end
