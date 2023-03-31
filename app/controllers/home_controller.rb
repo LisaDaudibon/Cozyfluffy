607899f583d4a509ef4ccd1cc313cbf7b02c0dc5
Suziwan <christe.suzanne@gmail.com>
2023-03-31 12:43:28 +0100

    SC add adoptables cats to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,10 @@ class HomeController < ApplicationController
 		# Scope your query to the dates being shown:
 		start_date = params.fetch(:date, Date.today).to_date
 		@events = Event.where(date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
-	  end
+	
+    # To display cats to home page
+    @cats = Cat.all
+    @catsresidents = Cat.where(adoption: false)
+    @catsadoptables = Cat.where(adoption: true)
+  end
 end
