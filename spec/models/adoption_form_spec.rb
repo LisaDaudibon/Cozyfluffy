4dd1b2f49c92c0b1ecc9a2655cd3695ddea6caaf
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-03-31 10:43:03 +0200

    LD test adoption_form model

--- spec/models/adoption_form_spec.rb
+++ spec/models/adoption_form_spec.rb
@@ -1,30 +1,45 @@
 require 'rails_helper'
 require 'date'
 
-RSpec.describe Event, type: :model do
+RSpec.describe AdoptionForm, type: :model do
   before do 
-    @cat = User.create(pseudo: "John", email: "lisa@yopmail.com")
+    @cat = Cat.create(name:"Lapon", birth_date:Date.new(2017,3,12))
   end
 
   describe 'fields validation' do
-    it 'needs title' do
-      expect { Event.create!(title: nil, user:@user, date: DateTime.now) }
-        .to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Title can't be blank")
+    it 'needs first_name' do
+      expect { AdoptionForm.create!(first_name: nil, last_name: "Maxime", email: "lisa@yopmail.com", phone_number: "06 05 45 57 67", cat:@cat) }
+        .to raise_error(ActiveRecord::RecordInvalid, "Validation failed: First name can't be blank")
     end
 
-    it 'needs date' do
-      expect { Event.create!(title: "bonjour", user:@user, date: nil) }
-        .to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Date can't be blank")
+    it 'needs last_name' do
+      expect { AdoptionForm.create!(first_name: "John", last_name: nil, email: "lisa@yopmail.com", phone_number: "06 05 45 57 67", cat:@cat) }
+        .to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Last name can't be blank")
     end
 
-    it 'needs user' do
-      expect { Event.create!(title: "bonjour", user: nil, date: DateTime.now) }
-        .to raise_error(ActiveRecord::RecordInvalid, "Validation failed: User must exist")
+    it 'needs email' do
+      expect { AdoptionForm.create!(first_name: "John", last_name: "Maxime", email: nil, phone_number: "06 05 45 57 67", cat:@cat) }
+        .to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Email can't be blank, Email n'a pas le bon format mon coco ! ")
     end
 
-    it 'build event' do
-      e = Event.new(title: "bonjour", user:@user, date: DateTime.now)
-      expect(e).to be_valid
+    it 'needs email to have the right format' do
+      expect { AdoptionForm.create!(first_name: "John", last_name: "Maxime", email: "lisa@yopmail.", phone_number: "06 05 45 57 67", cat:@cat) }
+        .to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Email n'a pas le bon format mon coco ! ")
+    end
+
+    it 'needs phone_number' do
+      expect { AdoptionForm.create!(first_name: "John", last_name: "Maxime", email: "lisa@yopmail.com", phone_number: nil, cat:@cat) }
+        .to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Phone number can't be blank")
+    end
+
+    it 'needs cat' do
+      expect { AdoptionForm.create!(first_name: "John", last_name: "Maxime", email: "lisa@yopmail.com", phone_number: "06 05 45 57 67", cat:nil) }
+        .to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Cat must exist")
+    end
+
+    it 'build adoption_form' do
+      a = AdoptionForm.new(first_name: "John", last_name: "Maxime", email: "lisa@yopmail.com", phone_number: "06 05 45 57 67", cat:@cat)
+      expect(a).to be_valid
     end
   end
 end
