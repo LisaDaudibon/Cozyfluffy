66e46bf7eed4c09753b7173dc49ddaa4e2ffc6d3
Chandara_LOEK <chandara.loek@outlook.com>
2023-03-31 14:17:48 +0200

    CL brunch mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,13 +13,23 @@ class UserMailer < ApplicationMailer
       mail(to: @user.email, subject: 'Bienvenue chez nous !') 
     end
 
-      def event_email(user, event)
-        @user = user 
-        @event = event
-        #on définit une variable @url qu'on utilisera dans la view d’e-mail
-        @url  = "https://git.heroku.com/cozyfluffy.git"
+    def event_email(user, event)
+      @user = user 
+      @event = event
+      #on définit une variable @url qu'on utilisera dans la view d’e-mail
+      @url  = "https://git.heroku.com/cozyfluffy.git"
     
-        # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-        mail(to: user.email, subject: 'Nouvel événement à venir !')
-      end
+      # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+      mail(to: user.email, subject: 'Nouvel événement à venir !')
+    end
+
+    def brunch_email(mail, name)
+      @name = name 
+      #on définit une variable @url qu'on utilisera dans la view d’e-mail
+      @url  = "https://git.heroku.com/cozyfluffy.git"
+    
+      # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
+      mail(to: mail, subject: 'Bon cadeau brunch')
+    end
+    
 end
